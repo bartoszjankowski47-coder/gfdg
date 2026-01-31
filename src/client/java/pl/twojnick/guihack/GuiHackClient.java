42bfb208bd9eaff26efcda4172d608fa644ae82b
bartoszjankowski47-coder <bartoszjankowski47@gmail.com>
2026-02-01 00:59:34 +0100

    upload fabric project

---
+++ src/client/java/pl/twojnick/guihack/GuiHackClient.java
@@ -0,0 +1,10 @@
+package pl.twojnick.guihack;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GuiHackClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
